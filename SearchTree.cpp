d99b5e508a90898340b370764c454182a1b3fe21
keysha-adesi <keyshaputrii590@gmail.com>
2026-06-11 11:04:14 +0700

    menambahkan Menghubungkan node baru ke child kiri atau kanan

--- SearchTree.cpp
+++ SearchTree.cpp
@@ -63,7 +63,25 @@ class BinaryTree
 
     // Thi function searches the current node of the specified node
     //as well as the current node of its parent
-    
+    void search(string element, Node *&parent, Node *&currentNode)
+    {
+        currentNode = ROOT;
+        parent = NULL;
+
+        while (currentNode != NULL && currentNode->info != element)
+        {
+            parent = currentNode;
+
+            if (element < currentNode->info)
+            {
+                currentNode = currentNode->leftcild;
+            }
+            else
+            {
+                currentNode = currentNode->rightchild;
+            }
+        }
+    }
 
 
 }
